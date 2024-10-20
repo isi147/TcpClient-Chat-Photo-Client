9d3258182366797f8bc52ca66f8286250a7164cd
Seyidmammadli_i <Seyidmammadli_i@STHQ0116-01>
2024-10-20 12:27:29 +0400

    Add project files.

---
+++ ConsoleApp48/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Sockets;
+
+var ipString = "127.0.0.1";
+var port = 27001;
+
+using var client = new TcpClient(ipString, port);
+
+var serverStream = client.GetStream();
+
+var fileStream = new FileStream("nerbala.jpg", FileMode.Create, FileAccess.Write);
+serverStream.CopyTo(fileStream);
+fileStream.Flush();
